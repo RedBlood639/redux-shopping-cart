41b9f01f1b54f20b92eb08e220659bf65dcc2002
Redblood639 <albinorabbithunter@gmail.com>
2022-09-19 18:40:56 +0900

    feat : Combining Date from Redux Slice

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createSelector } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
 
 export interface CartState {
   items: {
@@ -13,7 +14,38 @@ const initialState: CartState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      if (state.items[id]) {
+        state.items[id]++;
+      } else {
+        state.items[id] = 1;
+      }
+    },
+  },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
+
+export function getNumItems(state: RootState) {
+  let numItems = 0;
+  console.log("getNumItems");
+  for (let id in state.cart.items) {
+    numItems += state.cart.items[id];
+  }
+  return numItems;
+}
+
+export const getMemoizedNumItems = createSelector(
+  (state: RootState) => state.cart.items,
+  (items) => {
+    console.log("getMemoizedNumItems");
+    let numItems = 0;
+    for (let id in items) {
+      numItems += items[id];
+    }
+    return numItems;
+  }
+);
